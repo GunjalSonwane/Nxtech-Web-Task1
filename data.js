1145a24ecc3e76c666d07a630a78ca727f4ab98a
GunjalSonwane <140810571+GunjalSonwane@users.noreply.github.com>
2023-08-16 08:22:28 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: 'Dandelions',
+        path: 'musics/Song1.mp3',
+        artist: 'Ruth B',
+        cover: 'images/cover1.png'
+    },
+    {
+        name: 'Namo Namo',
+        path: 'musics/Song2.mp3',
+        artist: 'Amit Trivedi',
+        cover: 'images/cover2.png'
+    },
+    {
+        name: 'Night Changes',
+        path: 'musics/Song3.mp3',
+        artist: 'One Direction',
+        cover: 'images/cover3.png'
+    },
+    {
+        name: 'Apna Bana Le',
+        path: 'musics/Song4.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images/cover4.png'
+    },
+    {
+        name: 'Kesariya',
+        path: 'musics/Song5.mp3',
+        artist: 'Arijit singh',
+        cover: 'images/cover5.png'
+    },
+    
+]
